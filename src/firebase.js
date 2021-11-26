0697dc5f030de38d0f6735caf2cc3c165411a97c
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-11-26 17:11:03 -0300

    Running firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-import firebase from "firebase";
-// import "firebase/firestore";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDezn07v6zrsziRQYBhWg42EPB3eCYTRa0",
-//     authDomain: "second-firebase-6a3fe.firebaseapp.com",
-//     projectId: "second-firebase-6a3fe",
-//     storageBucket: "second-firebase-6a3fe.appspot.com",
-//     messagingSenderId: "667417469035",
-//     appId: "1:667417469035:web:041b813ccf98423de1512e"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyDezn07v6zrsziRQYBhWg42EPB3eCYTRa0",
+    authDomain: "second-firebase-6a3fe.firebaseapp.com",
+    projectId: "second-firebase-6a3fe",
+    storageBucket: "second-firebase-6a3fe.appspot.com",
+    messagingSenderId: "667417469035",
+    appId: "1:667417469035:web:041b813ccf98423de1512e"
+  };
   
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// //Exporta la funcionalidad de la base de datos
-// export const firebase = firebase.firestore();
-// //Exporta el paquete firebase para otros usos
-// export default firebase;
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// Exporta la funcionalidad de la DB
+export const firestore = firebase.firestore()
+// exporta el paquete de firebase para poder usarlo
+export default firebase
