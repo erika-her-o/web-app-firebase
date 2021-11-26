1bb94fe64596542659947524a9de9cd4265f9937
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-11-26 20:23:51 -0300

    Working tweet function

--- src/App.js
+++ src/App.js
@@ -4,37 +4,34 @@ import { firestore } from "./firebase";
 
 export default function App() {
   const [tweets, setTweets] = useState([]);
-
   useEffect(() => {
-  firestore.collection("tweets")
-  .get()
-  .then((snapshot) => {
-    const tweets = snapshot.docs.map((doc) => {
-      return {
-        tweet: doc.data().tweet,
-        autor: doc.data().autor,
-        id: doc.id
-      }
-    })
-    setTweets(tweets);
-    });
-  }, [])
- }
+    firestore
+      .collection("tweets")
+      .get()
+      .then((snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          return {
+            tweet: doc.data().tweet,
+            autor: doc.data().autor,
+            id: doc.id
+          };
+        });
+        setTweets(tweets);
+      });
+  }, []);
 
-
-
-  return (
-    <div className="App">
-      <h1>Tweets:</h1>
-      {tweets.map((tweet) => {
-        return (
-          <div key={tweet.id}>
-            <h1>{tweet.tweet}</h1>
-            <h4>por: {tweet.autor}</h4>
-          </div>
-        );
-      })}
-    </div>
-  );
-
+ return (
+  <div className="App">
+    <h1>Tweets:</h1>
+    {tweets.map((tweet) => {
+      return (
+        <div key={tweet.id}>
+          <h1>{tweet.tweet}</h1>
+          <h4>por: {tweet.autor}</h4>
+        </div>
+      );
+    })}
+  </div>
+);
+}
 
